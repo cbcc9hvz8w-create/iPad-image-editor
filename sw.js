dae0fc84ab76945f2be85c29f413c2adfe632046
cbcc9hvz8w-create <cbcc9hvz8w@privaterelay.appleid.com>
2026-08-16 23:16:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const APP_VERSION = "v1.4.0"; // build:version
+const APP_VERSION = "v1.5.0"; // build:version
 const CACHE_PREFIX = "snapcanvas-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const META_CACHE = "snapcanvas-update-meta";
 const SELECTED_KEY = "./__selected_shell__";
 const PRECACHE = [
   "./.nojekyll",
-  "./assets/pages-2glhZduv.js",
+  "./assets/pages-CyLRumu5.js",
   "./assets/pages-Di5R5kz-.css",
   "./favicon.svg",
   "./icons/app-icon.svg",
